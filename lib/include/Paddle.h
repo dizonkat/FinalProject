8b3c687b360602bf9cab46e52e67ec5e98e8f429
litfzz <oishanichatterji@gmail.com>
2023-12-07 12:05:54 -0500

    paddles DON'T go of screen, hard coded for now tho

--- lib/include/Paddle.h
+++ lib/include/Paddle.h
@@ -4,7 +4,7 @@
 
 class Paddle {
 public:
-    Paddle(float startX, float startY, float width, float height, sf::Color color);
+    Paddle(float startX, float startY, float w, float h, sf::Color color);
     void update(float deltaTime);
     void draw(sf::RenderWindow& window);
     void moveUp();
@@ -13,4 +13,7 @@ public:
 private:
     sf::RectangleShape m_shape;
     sf::Vector2f m_velocity;
+    sf::Vector2f position;
+    float height;
+    float width;
 };
--- lib/src/Paddle.cpp
+++ lib/src/Paddle.cpp
@@ -1,29 +1,42 @@
 #include "../include/Paddle.h"
+#include <iostream>
 
-Paddle::Paddle(float startX, float startY, float width, float height, sf::Color color) {
+Paddle::Paddle(float startX, float startY, float w, float h, sf::Color color) : width(w), height(h) {
     m_shape.setSize(sf::Vector2f(width, height));
     m_shape.setFillColor(color);
+    position = sf::Vector2f(startX, startY);
     m_shape.setPosition(startX, startY);
     m_velocity = sf::Vector2f(0.f, 0.f);
 }
 
 void Paddle::update(float deltaTime) {
     // Update the paddle's position based on its velocity
-    sf::Vector2f position = m_shape.getPosition();
+    //sf::Vector2f position = m_shape.getPosition();
     position += m_velocity * deltaTime;
     m_shape.setPosition(position);
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
     {
         // left key is pressed: move our character
-        Paddle::moveUp();
+        if (position.y >= 0 - height/2) {
+            Paddle::moveUp();
+        }
+        else {
+            m_velocity.y = 0;
+        }
     }
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
     {
         // right key is pressed: move our character
-        Paddle::moveDown();
+        if (position.y + height / 2 <= 600) {
+            Paddle::moveDown();
+        }
+        else {
+            m_velocity.y = 0;
+        }
     }
+    //std::cout << m_velocity.y
 }
 
 void Paddle::draw(sf::RenderWindow& window) {
