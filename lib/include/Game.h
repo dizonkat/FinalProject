f49720cdd0333448f9c3940fbe2e659426c6346b
dizonkat <dizon.ka@northeastern.edu>
2023-12-06 21:21:46 -0500

    fix game header and cmakelists

--- lib/include/Game.h
+++ lib/include/Game.h
@@ -1,30 +1,21 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Ball.h"
 #include "Paddle.h"
 
-Paddle::Paddle(float startX, float startY, float width, float height, sf::Color color) {
-    m_shape.setSize(sf::Vector2f(width, height));
-    m_shape.setFillColor(color);
-    m_shape.setPosition(startX, startY);
-    m_velocity = sf::Vector2f(0.f, 0.f);
-}
+class Game {
+public:
+    Game();
+    void run();
 
-void Paddle::update(float deltaTime) {
-    // Update the paddle's position based on its velocity
-    sf::Vector2f position = m_shape.getPosition();
-    position += m_velocity * deltaTime;
-    m_shape.setPosition(position);
-}
+private:
+    void processEvents();
+    void update(float deltaTime);
+    void render();
 
-void Paddle::draw(sf::RenderWindow& window) {
-    // Draw the paddle on the window
-    window.draw(m_shape);
-}
-
-void Paddle::moveUp() {
-    // Move the paddle up
-    m_velocity.y = -400.f; // Adjust the speed as needed
-}
-
-void Paddle::moveDown() {
-    // Move the paddle down
-    m_velocity.y = 400.f; // Adjust the speed as needed
-}
+    sf::RenderWindow m_window;
+    Ball m_ball;
+    Paddle m_paddle1;
+    Paddle m_paddle2;
+};
